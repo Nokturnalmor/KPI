ffaebad5896d2d36947e9fb0bc165bffe81b5b43
Nokturnalmor <76943309+Nokturnalmor@users.noreply.github.com>
2021-06-01 21:56:55 +0300

    Initial commit

---
+++ KPI.py
@@ -0,0 +1,32 @@
+import Cust_Functions as F
+
+from PyQt5 import QtWidgets, QtCore, QtGui
+from PyQt5.QtWinExtras import QtWin
+import os
+import time
+import subprocess
+from mydesign import Ui_MainWindow  # импорт нашего сгенерированного файла
+import sys
+
+
+class mywindow(QtWidgets.QMainWindow):
+    def __init__(self):
+        super(mywindow, self).__init__()
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+        self.ui.tabWidget.setCurrentIndex(0)
+
+
+app = QtWidgets.QApplication([])
+
+myappid = 'Powerz.BAG.SustControlWork.0.0.0'  # !!!
+QtWin.setCurrentProcessExplicitAppUserModelID(myappid)
+app.setWindowIcon(QtGui.QIcon(os.path.join("icons", "icon.png")))
+
+S = F.scfg('Stile').split(",")
+app.setStyle(S[1])
+
+application = mywindow()
+application.show()
+
+sys.exit(app.exec())
